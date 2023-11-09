2e532b893e79138f81ad9b782eabacb194e438ae
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2023-11-09 23:31:09 +0530

    Time: 3 ms (57.50%), Space: 7.1 MB (47.38%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int i=0;
-        while(i<s.size()){
-            if(s[i] != t[i]){
-                return t[i];
-            }
-            i++;
+       map<char,int> mp;
+        for(auto it: s){
+            mp[it]++;
         }
-        return t[i];
+        for(auto it:t){
+            mp[it]--;
+        }
+        for(auto it:mp){
+            if(it.second != 0){
+                return it.first;
+            }
+        }
+        return t[0];
     }
 };
