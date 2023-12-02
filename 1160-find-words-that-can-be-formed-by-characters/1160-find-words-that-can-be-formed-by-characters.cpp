e9fcb39e8db7d0f4831a0e68037e631323153d69
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2023-12-02 11:22:24 +0530

    Time: 44 ms (78.41%), Space: 25.4 MB (49.16%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int countCharacters(vector<string>& words, string chars) {
-        map<char,int> mp;
-        int len=0;
-        for(auto it:chars){
-            mp[it]++;
+    bool check(string s, vector<int> val){
+        for(int i=0;i<s.size();i++){
+            if(val[s[i]-'a']==0){
+                return false;
+            }
+            val[s[i]-'a']--;
         }
-        for(auto it: words){
-            map<char,int> mp2;
-            int flag=1;
-            for(auto i: it){
-                mp2[i]++;
+        return true;
+    }
+    int countCharacters(vector<string>& words, string chars) {
+        int len=0;
+        vector<int> ans(26,0);
+        for(int i=0;i<chars.size();i++){
+            ans[chars[i]-'a']++;
+        }
+        for(auto it : words){
+            if(check(it,ans)){
+                len+=it.size();
             }
-            for(auto i:mp2){
-                char ch=i.first;
-                if(i.second > mp[ch]){
-                    flag=0;
-                    break;
-                }
-            }
-            if(flag) len+=it.size();
         }
         return len;
     }
