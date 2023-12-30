5e1ba8f36f13df9739360468b38f90d80169bd1f
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2023-12-30 10:59:27 +0530

    Time: 22 ms (12.84%), Space: 12.8 MB (10.12%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool makeEqual(vector<string>& words) {
         int n = words.size();
-        int total =0;
         unordered_map<char,int> mp;
         for(auto it:words){
-            total += it.size();
             for(auto i : it){
                 mp[i]++;
             }
@@ -13,9 +11,6 @@ public:
         for(auto it:mp){
             if(it.second%n != 0)
                 return false;
-        }
-        if(total %n != 0){
-            return false;
         }
         return true;
     }
