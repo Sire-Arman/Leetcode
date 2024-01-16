022955695c9f4c26e84d30ebe39b0bf37665c7b6
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-01-17 02:39:33 +0530

    Time: 5 ms (34.26%), Space: 6.2 MB (74.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,24 @@
+#define a_int long long int
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        a_int s =0,e=x;
+        a_int mid ;
+        a_int temp = -1;
+        while(s<=e){
+            mid = s + (e-s)/2;
+            if(mid*mid > x){
+                e = mid-1;
+            }
+            else if( mid* mid <x){
+                temp = mid;
+                s = mid+1;
+            }
+            else if(mid*mid == x) {
+                return (int)mid;
+            }
+        }
+        
+        return temp;
     }
 };
