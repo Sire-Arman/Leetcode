be32b759efdbff30375e3f5be93ee3878b179f0b
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-03-02 14:44:37 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (46.92%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -21,9 +21,25 @@ public:
         dp[i] = mini;
         return dp[i];
     }
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
+    int tab(vector<int>& days, vector<int>& costs, int i){  
         int n = days.size();
-        vector<int> dp(n+1,-1);
-        return solve(days,costs,0,dp);
+        vector<int> dp(n+1,INT_MAX);
+        dp[n] = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            dp[i] = min(dp[i],costs[0] + dp[i+1]);
+            int j;
+        for(j=i;j<days.size() && days[j]< days[i]+7;j++);
+        
+        dp[i] = min(dp[i],costs[1] + dp[j]);
+        
+        for(j=i;j<days.size() && days[j]< days[i]+30;j++);
+        
+        dp[i] = min(dp[i],costs[2] + dp[j]);
+        }
+        return dp[0];
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        return tab(days,costs,0);
     }
 };
