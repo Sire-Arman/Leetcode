1825ef26e55b150ab80b70eda1e1890ebf5f8e25
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-03-04 15:10:32 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (5.01%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -9,8 +9,23 @@ public:
         }
         return dp[i][j] = ans;
     }
+     int tab(vector<int> &v){
+        int n = v.size();
+        vector<vector<int>> dp(50,vector<int> (50,0));
+         
+         for(int i=n-1;i>=0;i--){
+             for(int j = i+2;j<n;j++){
+                int ans = INT_MAX;
+                for(int k = i+1;k<j;k++){
+                    ans = min(ans, v[i]*v[j]*v[k] + dp[i][k] + dp[k][j]);
+                }
+                 dp[i][j] = ans;
+             }
+         }        
+        return dp[0][n-1];
+    }
     int minScoreTriangulation(vector<int>& values) {
-        vector<vector<int>> dp(50,vector<int>(50,-1));
-        return solve(values, 0, values.size()-1, dp);
+        // vector<vector<int>> dp(50,vector<int>(50,-1));
+        return tab(values);
     }
 };
