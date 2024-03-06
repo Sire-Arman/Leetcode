34ab9e38a1375054f08d053336bf67057d696ad3
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-03-06 20:50:53 +0530

    Time: 14 ms (49.20%), Space: 23.4 MB (8.30%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -21,11 +21,25 @@ public:
          }
          return dp[0][0];
     }
+    int solveSO(vector<int> &sat){
+         int n = sat.size();
+        vector<int> curr(n+1,0);
+        vector<int> next(n+1,0);
+         for(int i = n-1;i>=0;i--){
+             for(int j=i;j>=0;j--){
+                int inc = sat[i]*(j+1) + next[j+1];
+                int exc = next[j];
+                curr[j] = max(inc , exc);
+             }
+             next = curr;
+         }
+         return curr[0];
+    }
     
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
         int n = satisfaction.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-         return solveTab(satisfaction);
+         return solveSO(satisfaction);
     }
 };
