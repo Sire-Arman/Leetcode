ee84f227a477edb30019c126b2996b7b13fd959a
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-03-08 21:26:15 +0530

    Time: 8 ms (82.54%), Space: 25.3 MB (6.65%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -9,12 +9,26 @@ public:
         int nt = solve(i+1,j,s,n,dp);
         return dp[i][n] = max(nt,tk);
     }
+    int solveTab(vector<int>& s, int st,int end){
+        int t = s.size();
+        vector<vector<int>> dp(t+2,vector<int>(t+2,0));
+        // t /=3;
+        for(int i = end;i>=st;i--){
+            for(int n = 1;n<=t/3;n++){
+                int tk = s[i] + dp[i+2][n-1];
+                // int tk = 0;
+                int nt = dp[i+1][n];
+                dp[i][n] = max(nt,tk);
+            }
+        }
+        return dp[st][t/3];
+    }
     int maxSizeSlices(vector<int>& s) {
         int n = s.size();
-        vector<vector<int>> dp1(n+1,vector<int>(n/3+1,-1));
-        vector<vector<int>> dp2(n+1,vector<int>(n/3+1,-1));
-        int a = solve(0,n-2,s,n/3,dp1);
-        int b = solve(1,n-1,s,n/3,dp2);
+        // vector<vector<int>> dp1(n+1,vector<int>(n/3+1,-1));
+        // vector<vector<int>> dp2(n+1,vector<int>(n/3+1,-1));
+        int a = solveTab(s,0,n-2);
+        int b = solveTab(s,1,n-1);
         return max(a,b);
     }
 };
