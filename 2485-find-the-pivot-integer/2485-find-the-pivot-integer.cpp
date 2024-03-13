df41425ddbae3ffc418011246231a85f3d5564ff
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-03-13 14:36:59 +0530

    Time: 3 ms (51.39%), Space: 7.2 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
-    bool isPerfectSquare(int x)
-    {
-        if (x >= 0) {
-            long long sr = sqrt(x);
-            return (sr * sr == x);
-        }
-        return false;
-    }
     int pivotInteger(int n) {
-        long long sumr = n*(n+1)/2;
-        // cout<<sumr<<' '<<sqr<<endl;
-        
-        if(isPerfectSquare(4*sumr) ){
-             return sqrt(1+4*sumr)/2;
-            
-        }
-        else return -1;
+        long long sumr = n*(n+1)/2,sqr = sqrt(4*sumr);return (sqr*sqr) == 4*sumr?sqrt(1+4*sumr)/2:-1;
     }
 };
