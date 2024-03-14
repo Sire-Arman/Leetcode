0fc18ddf62a6e9bb7ac06b80d3f07d8c15f6898d
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-03-14 13:54:05 +0530

    Time: 3 ms (17.53%), Space: 7.3 MB (11.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,14 +9,15 @@ int helper(int n,vector<int>& dp ){
     return dp[n] = a1+a2;
 }
     int helperTab(int n){
-        // if(n<=1) return 1;
-        vector<int> dp(n+1,0);
-        dp[0] = 1;
-        dp[1] = 1;
+        if(n<=1) return 1;
+        // vector<int> dp(n+1,0);
+        int a =1,b=1,c=0;
         for(int i = 2;i<=n;i++){
-             dp[i] = dp[i-1] + dp[i-2];
+             c = b + a;
+                a=b;
+                b=c;
         }
-    return dp[n];
+    return c;
 }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
