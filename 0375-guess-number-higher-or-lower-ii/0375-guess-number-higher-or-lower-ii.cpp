22f6771cbd7d660318971f8a985a85bed4f9c44a
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-03-24 02:03:57 +0530

    Time: 36 ms (86.43%), Space: 11.3 MB (5.63%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -9,8 +9,23 @@ public:
         }
         return dp[st][end] = ans;
     }
+    int solveTab(int n){
+        vector<vector<int>> dp(n+2,vector<int> (n+2,0));
+       
+        for(int i = n;i>=1;i--){
+            for(int j = i;j<=n;j++){
+                if(i == j) continue;
+                else{int ans = INT_MAX;                
+                for(int k = i;k<= j;k++){
+                 ans = min(ans, k+max(dp[i][k-1],dp[k+1][j]));   
+                }
+                dp[i][j] = ans;}
+            }
+        }
+        return dp[1][n];
+    }
     int getMoneyAmount(int n) {
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(0,n,dp);
+        return solveTab(n);
     }
 };
