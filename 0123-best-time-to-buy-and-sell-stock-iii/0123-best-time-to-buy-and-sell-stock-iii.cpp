d41d6b368c39329dbbd5050f5ec6e9f4dd55eef1
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-03-25 20:02:46 +0530

    Time: 772 ms (5.03%), Space: 341.1 MB (5.05%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -14,9 +14,31 @@ public:
         }
         return dp[i][b][lim] = ans;
     }
+        int solveTab(vector<int> &arr){
+        int n = arr.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3,0)));
+        
+        for(int i = n-1;i>=0;i--){
+            for(int j =0;j<=1;j++){
+                for(int k = 0;k<=1;k++){
+                    int ans = 0;
+                    if(j){
+                        ans = max(ans, dp[i+1][0][k] - arr[i]);
+                        ans = max(ans, dp[i+1][1][k]);
+                    }
+                    else{
+                        ans = max(ans, arr[i] + dp[i+1][1][k+1]);
+                        ans = max(ans, dp[i+1][0][k]);
+                    }
+                    dp[i][j][k] = ans;
+                }
+            }
+        }
+        return dp[0][1][0];
+    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (2,-1)));
-        return solve(prices,0,true,0,dp);
+        return solveTab(prices);
     }
 };
