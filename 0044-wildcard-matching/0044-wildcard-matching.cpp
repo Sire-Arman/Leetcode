f278ef6939888cc22453be53e432e9db5b91df49
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-03 00:19:10 +0530

    Time: 26 ms (83.88%), Space: 10.6 MB (95.97%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -30,34 +30,39 @@ public:
      bool solveTab(string& s, string &p){
         
          int n = s.length() ,m = p.length();
-         vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-         dp[n][m] = 1;
+         vector<int> prev(m+1,0);
+         vector<int> curr(m+1,0);
+         // curr[m] = 1;
+         prev[m] = 1;
+         // vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+         // dp[n][m] = 1;
          for(int j=0;j<p.length();j++){
              bool flag = true;
               for(int k = p.length()-1;k>=j;k--){
                 if(p[k] != '*') {flag = false;break;}
               }
-            dp[n][j] = flag;
+            prev[j] = flag;
          }
          for(int i = n-1;i>=0;i--){
              for(int j=m-1;j>=0;j--){
                     bool ans = false;
                     if(s[i] == p[j] || p[j] == '?'){
-                        ans = ans || dp[i+1][j+1];
+                        ans = ans || prev[j+1];
                     }
                     else{
                         if(p[j] == '*'){
-                            ans = ans ||  dp[i][j+1];
-                                ans = ans || dp[i+1][j];
+                            ans = ans || curr[j+1];
+                                ans = ans || prev[j];
                         }
                         else{
                             ans = false;
                         }
                     }
-                    dp[i][j] = ans;
+                    curr[j] = ans;
              }
+             prev = curr;
          }
-       return dp[0][0];
+       return prev[0];
     }
     bool isMatch(string s, string p) {
         int n = s.length() ,m = p.length();
