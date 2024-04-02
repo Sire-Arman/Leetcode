3a5c1d9d797c5203cffda12075fb8388b2285635
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-03 00:12:41 +0530

    Time: 28 ms (80.20%), Space: 31.5 MB (14.63%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -27,9 +27,41 @@ public:
         }
         return dp[i][j] = ans;
     }
+     bool solveTab(string& s, string &p){
+        
+         int n = s.length() ,m = p.length();
+         vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+         dp[n][m] = 1;
+         for(int j=0;j<p.length();j++){
+             bool flag = true;
+              for(int k = p.length()-1;k>=j;k--){
+                if(p[k] != '*') {flag = false;break;}
+              }
+            dp[n][j] = flag;
+         }
+         for(int i = n-1;i>=0;i--){
+             for(int j=m-1;j>=0;j--){
+                    bool ans = false;
+                    if(s[i] == p[j] || p[j] == '?'){
+                        ans = ans || dp[i+1][j+1];
+                    }
+                    else{
+                        if(p[j] == '*'){
+                            ans = ans ||  dp[i][j+1];
+                                ans = ans || dp[i+1][j];
+                        }
+                        else{
+                            ans = false;
+                        }
+                    }
+                    dp[i][j] = ans;
+             }
+         }
+       return dp[0][0];
+    }
     bool isMatch(string s, string p) {
         int n = s.length() ,m = p.length();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        return solve(s,p,0,0,dp);
+        // vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        return solveTab(s,p);
     }
 };
