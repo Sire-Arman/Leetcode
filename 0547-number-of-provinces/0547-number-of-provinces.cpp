ff2e813cbb0e7e9f739138c4a562e02e28a532e1
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-20 22:51:59 +0530

    Time: 13 ms (96.65%), Space: 18.1 MB (74.87%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& isc) {
-        vector<int> vis(isc.size(),0);
-        function<void(int)> dfs = [&](int node){
-            if(vis[node]==1) return;
+    void dfs(int node,vector<vector<int>>& isc,vector<int> &vis){
+        if(vis[node]==1) return;
             vis[node] = 1;
             for(int i=0;i<isc[node].size();i++){
-                if(isc[node][i] == 1) dfs(i);
+                if(isc[node][i] == 1) dfs(i,isc,vis);
             }
-        };
+    }
+    int findCircleNum(vector<vector<int>>& isc) {
+        vector<int> vis(isc.size(),0);
+        // function<void(int)> dfs = [&](int node){
+        //     if(vis[node]==1) return;
+        //     vis[node] = 1;
+        //     for(int i=0;i<isc[node].size();i++){
+        //         if(isc[node][i] == 1) dfs(i);
+        //     }
+        // };
         int pr =0;
         for(int i=0;i<isc.size();i++){
             if(vis[i]!= 1){
                 pr++;
-                dfs(i);
+                dfs(i,isc,vis);
             }
         }
         return pr;
