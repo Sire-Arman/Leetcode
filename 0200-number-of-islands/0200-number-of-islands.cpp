d4ca45601f931aabdf9dc23ba4ec290fe3c88d3a
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-21 16:33:55 +0530

    Time: 36 ms (19.11%), Space: 23.9 MB (12.20%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    void bfs(int r, int c, vector<vector<char>>& grid, vector<vector<int>>& vis){
+        int row = grid.size();
+        int col = grid[0].size();
+        vis[r][c] = 1;
+        queue<pair<int,int>> q;
+        q.push({r,c});
+        while(!q.empty()){
+            int rw = q.front().first;
+            int cl = q.front().second;
+            q.pop();
+            int nr = rw+1, nc = cl;
+                    if(nr >=0 && nc>=0 && nr <row && nc <col && grid[nr][nc] == '1' && !vis[nr][nc]){
+                        vis[nr][nc] = 1;
+                        q.push({nr,nc});
+                    }
+            nr = rw-1;
+            if(nr >=0 && nc>=0 && nr <row && nc <col && grid[nr][nc] == '1' && !vis[nr][nc]){
+                        vis[nr][nc] = 1;
+                        q.push({nr,nc});
+                    }
+            nc = cl+1, nr = rw;
+            if(nr >=0 && nc>=0 && nr <row && nc <col && grid[nr][nc] == '1' && !vis[nr][nc]){
+                        vis[nr][nc] = 1;
+                        q.push({nr,nc});
+                    }
+            nc = cl-1;
+            if(nr >=0 && nc>=0 && nr <row && nc <col && grid[nr][nc] == '1' && !vis[nr][nc]){
+                        vis[nr][nc] = 1;
+                        q.push({nr,nc});
+                    }
+                }
+    }
     void dfs(vector<vector<char>>& grid,int row, int col){
         if(row<0 ||col<0|| row>= grid.size() || col>= grid[0].size() || grid[row][col] == '0') return;
             grid[row][col] = '0';
@@ -17,12 +49,15 @@ public:
         //     dfs(row-1,col);
         //     dfs(row,col-1);  
         // };
+            int n = grid.size();
+            int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int> (m,0));
         int cnt =0;
         for(int i=0;i<grid.size();i++){
             for(int j =0;j<grid[0].size();j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid,i,j);
+                if(!vis[i][j] && grid[i][j] == '1'){
                     cnt++;
+                    bfs(i,j,grid,vis);
                 }
             }
         }
