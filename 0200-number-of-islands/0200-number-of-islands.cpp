bf7eab4e284105ed1e0e50132f3fde91feb392d1
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-21 16:03:06 +0530

    Time: 27 ms (68.54%), Space: 15.7 MB (92.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        function<void(int ,int)> dfs = [&](int row, int col){
-            if(row<0 ||col<0|| row>= grid.size() || col>= grid[0].size() || grid[row][col] == '0') return;
+    void dfs(vector<vector<char>>& grid,int row, int col){
+        if(row<0 ||col<0|| row>= grid.size() || col>= grid[0].size() || grid[row][col] == '0') return;
             grid[row][col] = '0';
-            dfs(row+1,col);
-            dfs(row,col+1);
-            dfs(row-1,col);
-            dfs(row,col-1);  
-        };
+            dfs(grid,row+1,col);
+            dfs(grid,row,col+1);
+            dfs(grid,row-1,col);
+            dfs(grid,row,col-1);  
+        }
+    int numIslands(vector<vector<char>>& grid) {
+        // function<void(int ,int)> dfs = [&](int row, int col){
+        //     if(row<0 ||col<0|| row>= grid.size() || col>= grid[0].size() || grid[row][col] == '0') return;
+        //     grid[row][col] = '0';
+        //     dfs(row+1,col);
+        //     dfs(row,col+1);
+        //     dfs(row-1,col);
+        //     dfs(row,col-1);  
+        // };
         int cnt =0;
         for(int i=0;i<grid.size();i++){
             for(int j =0;j<grid[0].size();j++){
                 if(grid[i][j] == '1'){
-                    dfs(i,j);
+                    dfs(grid,i,j);
                     cnt++;
                 }
             }
