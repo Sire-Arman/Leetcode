99666e5762e9cd9dcf54196daa59758bfacaa6d5
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-24 18:47:54 +0530

    Time: 18 ms (56.05%), Space: 16 MB (94.89%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,31 +1,57 @@
 class Solution {
 public:
-    bool bfs(int node,vector<int>& vis, vector<vector<int>>& graph){
+    bool dfs(int node,vector<int>& vis, vector<vector<int>>& graph){
+        // if(vis[node] != -1){
+        //     if(node != par && vis[node] == vis[par]) return false;
+        //     else return true;
+        // } 
+        // if(par == -1) vis[node] = 0;
+        // else vis[node] = !vis[par];
+        bool ans = true;
+        for(auto it : graph[node]){
+            if(vis[it] == -1) { 
+                vis[it] = !vis[node]; 
+                ans = ans && dfs(it,vis,graph);
+            }
+            else{
+                if(it != node && vis[it] == vis[node]) return false;
+            }
+        }
+        return ans;
+    }
+    bool bfs(int node, vector<int>& vis, vector<vector<int>>& graph){
         queue<int>q;
         q.push(node);
         vis[node] = 0;
         while(!q.empty()){
-            int n = q.front();
+            auto n = q.front();
             q.pop();
-            for(int nbr : graph[n]){
+            for(auto nbr : graph[n]){
                 if(vis[nbr] == -1){
-                    vis[nbr] = !vis[n];
+                    if(vis[n] == 0) vis[nbr] = 1;
+                    else vis[nbr] = 0;
                     q.push(nbr);
                 }
-                else if((vis[nbr] ==1 && vis[n] ==1) || (vis[nbr] == 0 && vis[n] == 0)){
+                else{
+                    if(vis[nbr] != -1 && vis[nbr] == vis[n]){
                         return false;
                     }
+                }
             }
         }
         return true;
+        
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> vis(n,-1);
         for(int i=0;i<n;i++){
-            if(vis[i] == -1 && bfs(i,vis,graph) == false) return false;
-
+            
+            if(vis[i] == -1){
+                vis[i] = 0;
+                if(dfs(i,vis,graph) == false) return false;
+            }
         }
-        return true; 
+        return true;
     }
 };
