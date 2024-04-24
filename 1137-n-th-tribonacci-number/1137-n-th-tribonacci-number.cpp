ac0264b64407d405661d5505813c455cc5ea8f79
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-24 17:56:50 +0530

    Time: 3 ms (22.77%), Space: 7.1 MB (81.41%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -3,12 +3,17 @@ public:
     int tribonacci(int n) {
         if(n==0) return 0;
         if(n<=2) return 1;
-        vector<int> dp(n+1,0);
-        dp[1] = 1;
-        dp[2] = 1;
+        int a =0,b=1,c=1;
+        // vector<int> dp(n+1,0);
+        // dp[1] = 1;
+        // dp[2] = 1;
         for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+            int x = a+b+c;
+            a=b;
+            b=c;
+            c=x;
+            
         }
-        return dp[n];
+        return c;
     }
 };
