f1a4622fce7f56e016686b4a3d79dac239f481eb
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-27 18:12:15 +0530

    Time: 24 ms (51.89%), Space: 28.4 MB (28.54%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int helper(int in_index, int pos, unordered_map<char, vector<int>>& positions, string& key, string& ring, vector<vector<int>>& memo) {
-        if (in_index == key.size()) {
-            return 0;
-        }
-        if (memo[in_index][pos] != -1) {
-            return memo[in_index][pos];
-        }
-        int min_steps = INT_MAX;
-        for (int i : positions[key[in_index]]) {
-            int a = abs(pos-i);
-            int steps =  min(a, static_cast<int>(ring.size()- a));
-            int next_steps = helper(in_index + 1, i, positions, key, ring, memo);
-            min_steps = min(min_steps, steps + next_steps);
-        }
-        
-        return memo[in_index][pos] = min_steps + 1;
+    int helper(string& ring, string& key, int i, int pos, unordered_map<char,vector<int>>& mp, vector<vector<int>>& dp){
+        if(i == key.size()) return 0;
+        if(dp[i][pos] != -1) return dp[i][pos];
+        int ans = INT_MAX;
+         for(auto it : mp[key[i]]){
+             int step = min(abs(pos-it), static_cast<int>(ring.size() - abs(pos-it)));
+             // cout<<step<<endl;
+             ans = min(ans, step + helper(ring,key, i+1,it,mp,dp));
+         }
+        return dp[i][pos] = ans;
     }
     int findRotateSteps(string ring, string key) {
         unordered_map<char,vector<int>> mp;
         for(int i=0;i<ring.size();i++){
             mp[ring[i]].push_back(i);
         }
-        vector<vector<int>> dp(101,vector<int>(101,-1));
-        return helper(0,0,mp,key,ring,dp);
+        vector<vector<int>> dp(101, vector<int>(101,-1));
+        return helper(ring, key, 0,0,mp, dp) + key.size();
     }
 };
