214b422b2c31af9009637e182f8b28a873bbca34
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-04-30 03:13:02 +0530

    Time: 11 ms (92.51%), Space: 16.7 MB (87.51%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -13,19 +13,43 @@ public:
         }
         return (s == indeg.size());
     }
+    bool dfs(vector<int> adj[], vector<int>& vis, vector<int>& path, int node){
+        vis[node] =1;
+        path[node] =1;
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if (dfs(adj,vis,path,it)) return true;
+            }
+            else{
+                if(path[it]) return true;
+            }
+        }
+        path[node] =0;
+        return false;
+    }
     bool canFinish(int n, vector<vector<int>>& pr) {
         vector<int> adj[n];
-        vector<int> indeg(n,0);
-        queue<int> q;
+        // vector<int> indeg(n,0);
+        // queue<int> q;
         for(int i=0;i<pr.size();i++){
            adj[pr[i][1]].push_back(pr[i][0]);
         }
+        // for(int i=0;i<n;i++){
+        //     for(auto it : adj[i]){
+        //         indeg[it]++;
+        //     }
+        // }
+        // for(int i=0;i<n;i++) if(indeg[i] == 0) q.push(i);
+        // return kahnalgo(adj,indeg,q);
+        
+        
+        vector<int> vis(n,0);
+        vector<int> path(n,0);
         for(int i=0;i<n;i++){
-            for(auto it : adj[i]){
-                indeg[it]++;
+            if(!vis[i]){
+                if(dfs(adj,vis,path,i)) return false;
             }
         }
-        for(int i=0;i<n;i++) if(indeg[i] == 0) q.push(i);
-        return kahnalgo(adj,indeg,q);
+        return true;
     }
 };
