2cdc0e0fbb8655aa5515ce7f19b2d0d7683d33a5
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-05-06 12:12:09 +0530

    Time: 280 ms (47.85%), Space: 159.7 MB (97.40%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -10,34 +10,25 @@
  */
 class Solution {
 public:
+//     reverse LL and return new head
     ListNode* reverse(ListNode* head){
-        
         ListNode* prev = NULL;
         ListNode* curr = head;
+        
+        
         while(curr != NULL){
             ListNode* temp = curr->next;
             curr->next = prev;
             prev = curr;
             curr = temp;
         }
+        
+        
         return prev;
     }
-    ListNode* removeNodes(ListNode* head) {
-        ListNode* rHEAD = reverse(head);
-        ListNode* temp = rHEAD;
-        int maxi = INT_MIN;
-        while(temp != NULL){
-            // cout<<temp->val<<' ';
-            if(temp->val >= maxi){
-                maxi = temp->val;
-            }
-            else{
-                temp->val = -1;
-            }
-            temp = temp->next;
-        }
-        head = reverse(rHEAD);
-        temp = NULL;
+//     Remove Nodes
+    ListNode* remove(ListNode* head){
+        ListNode* temp = NULL;
         ListNode* next = head;
         while(next != NULL){
             if(next->val == -1){
@@ -50,5 +41,28 @@ public:
             next = next->next;
         }
         return head;
+    }
+    
+    
+    ListNode* removeNodes(ListNode* head) {
+//         rHEAD - reversed LL head pointer
+        ListNode* rHEAD = reverse(head);
+        ListNode* temp = rHEAD;
+//         Compare val with previous max and check if they are to be removed or not
+        int maxi = INT_MIN;
+        while(temp != NULL){
+            // cout<<temp->val<<' ';
+            if(temp->val >= maxi){
+                maxi = temp->val;
+            }
+            else{
+                temp->val = -1;
+            }
+            temp = temp->next;
+        }
+//         Now reverse the LL again and remove all marked nodes
+        head = reverse(rHEAD);
+//         remove elements
+        return remove(head);
     }
 };
