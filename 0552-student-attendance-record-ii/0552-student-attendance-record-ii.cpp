b5d00a7afada156a5398099863c09a5b4fc79377
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-05-26 14:33:35 +0530

    Time: 2005 ms (5.12%), Space: 466.5 MB (10.56%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -15,8 +15,33 @@ public:
         ans =  (ans+solve(n-1,a,2,dp))%MOD;
         return dp[n][a][l] = ans;
     }
+    int solveTab(int n){
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                dp[0][i][j]=1;
+            }
+        }
+        for(int i =1;i<=n;i++){
+            for(int a =0;a<2;a++){
+                for(int l=0;l<3;l++){
+                     int ans =0;
+                    if(l){
+                        ans = (ans+dp[i-1][a][l-1])%MOD;
+                    }
+                    if(a){
+                        ans = (ans+dp[i-1][a-1][2])%MOD;
+                    }
+                    ans =  (ans+dp[i-1][a][2])%MOD;
+                    dp[i][a][l] = ans;
+                }
+            }
+        }
+        return dp[n][1][2];
+    }
     int checkRecord(int n) {
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(n,1,2,dp);
+        // return solve(n,1,2,dp);
+        return solveTab(n);
     }
 };
