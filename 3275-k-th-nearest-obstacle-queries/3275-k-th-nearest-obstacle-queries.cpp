f70e8cb9596884de0bbcd46f17180a51d29b2419
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-09-13 12:35:51 +0530

    Time: 688 ms (24.93%), Space: 288.2 MB (27.58%) - LeetHub

---
+++ 3275-k-th-nearest-obstacle-queries/3275-k-th-nearest-obstacle-queries.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<vector<int>>& queries, int k) {
+        priority_queue<int> pq;
+        vector<int> ans;
+        for(auto it : queries){
+            pq.push(abs(it[0])+abs(it[1]));
+            if(pq.size()<k){
+                ans.push_back(-1);
+            }
+            else{
+            while(pq.size() >k){
+                pq.pop();
+            }
+                ans.push_back(pq.top());
+            }
+        }
+        return ans;
+    }
+};
