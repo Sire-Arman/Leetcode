93ac2edd68d0b23a298b90f9f73cea4938956807
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2024-10-19 04:17:25 +0530

    Time: 11 ms (78.42%), Space: 10.1 MB (82.38%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int inEx(vector<int>& nums, int i,int curr, int maxi){
         if(i == nums.size()) return 0;
-        int ans =0;
-        ans += (maxi==(curr|nums[i])?1:0)+ inEx(nums,i+1,(curr|nums[i]),maxi);
-        ans += (maxi==(curr|nums[i])?1:0)+ inEx(nums,i+1,curr,maxi);
+        int ans = (maxi==(curr|nums[i])?1:0);
+        ans += inEx(nums,i+1,(curr|nums[i]),maxi);
+        ans += inEx(nums,i+1,curr,maxi);
         return ans;
     }
     int countMaxOrSubsets(vector<int>& nums) {
         int maxi =0;
         for(auto it : nums){
 maxi |= it;}
-        return inEx(nums,0,0,maxi)/2;
+        return inEx(nums,0,0,maxi);
     }
 };
