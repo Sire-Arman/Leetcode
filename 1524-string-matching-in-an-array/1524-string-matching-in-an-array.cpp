a8c2701a630fef3a151e0e9d5c198f3d530922e3
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2025-01-07 15:39:33 +0530

    Time: 12 ms (9.04%), Space: 16.2 MB (7.01%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,5 +1,57 @@
 class Solution {
 public:
+vector<int> findPrefix(string pattern)
+{
+    int m=pattern.size();
+    vector<int> prefix(m);
+    prefix[0]=0;
+    int len=0;
+    int j=0;
+    int i=1;
+    while(i<m){
+        if(pattern[i] == pattern[j]){
+            j++;
+            prefix[i] = j;
+            i++;
+        }
+        else{
+           if(j>0){
+               j = prefix[j-1];
+           }
+           else{
+               prefix[i] = 0;
+               i++;
+           }
+        }
+    }   
+    return prefix;
+}
+int KMP(string t, string p){
+    int n = t.size();
+    int m = p.size();
+    vector<int> prefix = findPrefix(p);
+    // for(auto it : prefix) cout<<it<<' ';
+    int j =0,i=0;
+    int ans = -1;
+    while(i<n){
+        if(t[i] == p[j]){
+            i++;
+            j++;
+            if(j==m){
+                ans = i-j;
+                return ans;
+            }
+        }
+        else{
+            if(j>0) j = prefix[j-1];
+            else{
+                i++;
+            }
+        }
+    }
+
+    return ans;
+}
 static bool cmp(string& a, string &b){
     return a.size()<b.size();
 }
@@ -7,9 +59,11 @@ static bool cmp(string& a, string &b){
         sort(words.begin(),words.end(),cmp);
         vector<string> ans;
         for(int i=0;i<words.size();i++){
-            string t = words[i];
+            string p = words[i];
+            // cout<<p<<endl;
             for(int j=i+1;j<words.size();j++){
-                if(words[j].find(t) != -1) {ans.push_back(t);break;}
+                string t = words[j];
+                if(KMP(t,p) != -1) {ans.push_back(p);break;}
             }
         }
         return ans;
