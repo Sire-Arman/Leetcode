05165ef4daf77eff1b0745e435710bbeb247986b
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2025-01-11 18:39:29 +0530

    Time: 487 ms (51%), Space: 201.5 MB (29%) - LeetHub

--- 3350-distribute-elements-into-two-arrays-ii/3350-distribute-elements-into-two-arrays-ii.cpp
+++ 3350-distribute-elements-into-two-arrays-ii/3350-distribute-elements-into-two-arrays-ii.cpp
@@ -1,9 +1,9 @@
 #include <ext/pb_ds/assoc_container.hpp> // For PBDS
 using namespace __gnu_pbds;
 typedef tree<
-    int,
+    pair<int,int>,
     null_type,
-    less_equal<int>,
+    less<pair<int,int>>,
     rb_tree_tag,
     tree_order_statistics_node_update>
 ordered_set;
@@ -16,27 +16,27 @@ void solve(vector<int> &nums, ordered_set &s1, ordered_set &s2, vector<int> &a1)
     int n = nums.size();
     vector<int> a2;
     // Insert first two elements
-    s1.insert(nums[0]);
+    s1.insert({nums[0],0});
     a1.push_back(nums[0]);
-    s2.insert(nums[1]);
+    s2.insert({nums[1],1});
     a2.push_back(nums[1]);
 
     for (int i = 2; i < n; i++) {
-        int a = s1.size() - s1.order_of_key(nums[i] + 1); // Elements >= nums[i]
-        int b = s2.size() - s2.order_of_key(nums[i] + 1);
+        int a = s1.size() - s1.order_of_key({nums[i] + 1,-1}); // Elements >= nums[i]
+        int b = s2.size() - s2.order_of_key({nums[i] + 1,-1});
 
         if (a < b) {
-            s2.insert(nums[i]);
+            s2.insert({nums[i],i});
             a2.push_back(nums[i]);
         } else if (a > b) {
-            s1.insert(nums[i]);
+            s1.insert({nums[i],i});
             a1.push_back(nums[i]);
         } else {
             if (a1.size() <= a2.size()) {
-                s1.insert(nums[i]);
+                s1.insert({nums[i],i});
                 a1.push_back(nums[i]);
             } else {
-                s2.insert(nums[i]);
+                s2.insert({nums[i],i});
                 a2.push_back(nums[i]);
             }
         }
