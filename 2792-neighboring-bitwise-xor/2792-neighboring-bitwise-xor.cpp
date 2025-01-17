23478093a0f7ab19e067b14d3f685167b8fc17a0
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2025-01-17 22:54:14 +0530

    Time: 1 ms (70.56%), Space: 263.8 MB (90.19%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -15,19 +15,7 @@ public:
             curr = next;
         }
         if(st != curr) ans = false;
-        st =1,curr=1,next =0;
-         for(int i=0;i<n;i++){
-            if(d[i]==1){
-                next = !curr;
-            }
-            else{
-                next = curr;
-            }
-            curr = next;
-        }
-        if(st != curr) ans = false;
+
         return ans;
-
-
     }
 };
