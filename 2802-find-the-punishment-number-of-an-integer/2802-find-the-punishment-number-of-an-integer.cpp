4a1560b478d493f4d76f156d0710a5c2833a4dd7
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2025-02-15 15:15:18 +0530

    Time: 12 ms (76.83%), Space: 7.8 MB (95.72%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+bool sumCheck(int t, int num){
+    if(t==0 && num==0) return 1;
+    if(t<0) return 0;
+    int temp = num,div=10;
+    bool ans = false;
+    while((temp*10)/div){
+        int rem = temp%div;
+        ans = ans || sumCheck(t-rem,temp/div);
+        div*=10;
+    }
+    return ans;
+}
+
+    int punishmentNumber(int n) {
+        int ans =0;
+        for(int i=1;i<=n;i++){
+            if(sumCheck(i,i*i)) {ans += i*i;}
+        }
+        return ans;
+    }
+};
