05f2d45d6016f1a24b32bc187053922704d821a0
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-01-13 17:39:24 +0000

    Time: 954 ms (6.71%), Space: 472.5 MB (5.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-string expand(int l, int r, string s){
-    while(l>=0 && r <s.size() && s[l] == s[r]){
-        l--;
-        r++;
-    }
-    return s.substr(l+1,r-l-1);
-}
-    string longestPalindrome(string s) {
+    string isEPalindromic(string& s,int i){
         int n = s.size();
-        if(n<=1) return s;
-        
+        int j = i+1;
         string ans = "";
-        for(int i=0;i<n-1;i++){
-            string odd = expand(i,i,s);
-            string even = expand(i,i+1,s);
-            if(ans.length()< odd.length()){
-                ans = odd;
-            }
-            if(ans.length() < even.length()){
-                ans = even;
+        while(i>=0 && j<n){
+            if(s[i] != s[j]) break;
+            ans = s.substr(i,j-i+1);
+            j++;
+            i--;
+        }
+       return ans;
+    }
+    string isOPalindromic(string& s,int i){
+        int n = s.size();
+        int j = i;
+        string ans = s.substr(i,1);
+        while(i>=0 && j<n){
+            if(s[i] != s[j]) break;
+            ans = s.substr(i,j-i+1);
+            j++;
+            i--;
+        }
+       return ans;
+    }
+    // string solve(string& s, int i, int j){
+    // }
+    string longestPalindrome(string s) {
+        string ans = "";
+        for(int i =0;i<s.size();i++){
+            string res = isEPalindromic(s,i);
+            string res2 = isOPalindromic(s,i);
+            if(res.size()>ans.size() || res2.size()>ans.size()){
+                if(res2.size()>res.size()){
+                    ans=res2;
+                }
+                else ans = res;
             }
         }
         return ans;
