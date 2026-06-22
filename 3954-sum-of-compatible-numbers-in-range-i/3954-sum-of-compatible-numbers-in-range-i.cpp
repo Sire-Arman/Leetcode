279a8abb81b374208bcb8b3f8eb865d28c086350
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-06-22 13:33:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (75.04%) - LeetHub

--- 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.cpp
+++ 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    int recursion(int n, int k, int i){
+        if(i> n+k) return 0;
+        int ans = (i>0 && (n&i) == 0)?i:0;
+        return ans + recursion(n,k,i+1);
+    }
     int sumOfGoodIntegers(int n, int k) {
-        int ans = 0;
-        for(int i = n-k;i<=n+k;i++){
-            if(i>0 && (n&i) == 0) {
-                ans+=i;
-            }
-        }
-        return ans;
+        // int ans = 0;
+        // for(int i = n-k;i<=n+k;i++){
+        //     if(i>0 && (n&i) == 0) {
+        //         ans+=i;
+        //     }
+        // }
+        // return ans;
+        return recursion(n,k,n-k);
     }
 };
