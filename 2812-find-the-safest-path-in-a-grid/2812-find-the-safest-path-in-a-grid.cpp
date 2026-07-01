20193eeb0bded272f30ea87ab83a6e350a42aa3b
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-07-01 14:38:10 +0530

    Time: 320 ms (84.79%), Space: 129.8 MB (87.26%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -2,37 +2,52 @@ class Solution {
 public:
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int n = grid.size();
-        auto bounds_check = [n](int r, int c){ return r >= 0 && c >= 0 && r < n && c < n; };
-        deque<tuple<int, int, int>> q;
-        for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) {
-            if (grid[i][j]) q.emplace_back(i, j, 1);
-            --grid[i][j];
-        }
-        while (!q.empty()) {
-            auto [i, j, safety] = q.front(); q.pop_front();
-            array<pair<int, int>, 4> expand = {{{i+1, j}, {i-1, j}, {i, j+1}, {i, j-1}}};
-            for (auto [r, c] : expand) if (bounds_check(r, c) && grid[r][c] == -1) {
-                grid[r][c] = safety;
-                q.emplace_back(r, c, safety+1);
-            }
-        }
-        int min_safety = grid[0][0];
-        q.emplace_back(0, 0, grid[0][0]);
-        while (!q.empty()) {
-            auto [i, j, safety] = q.front(); q.pop_front();
-            min_safety = min(min_safety, safety);
-            if (i == n-1 && j == n-1) break;
-            array<pair<int, int>, 4> expand = {{{i+1, j}, {i-1, j}, {i, j+1}, {i, j-1}}};
-            for (auto [r, c] : expand) if (bounds_check(r, c) && grid[r][c] != -1) {
-                int safety = grid[r][c];
-                grid[r][c] = -1;
-                if (safety < min_safety) {
-                    q.emplace_back(r, c, safety);
-                } else {
-                    q.emplace_front(r, c, safety);
+        int ans = 0;
+        vector<vector<int>> dis(n,vector<int>(n,INT_MAX));
+        vector<vector<bool>> vis(n,vector<bool>(n,false));
+        priority_queue<tuple<int,int,int>> pq;
+        queue<pair<int,int>> q;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]){
+                    q.push({i,j});
+                    dis[i][j] = 0;
                 }
             }
         }
-        return min_safety;
+        vector<int> dx = {-1,0,1,0};
+        vector<int> dy = {0,1,0,-1};
+        while(!q.empty()){
+            auto [x,y] = q.front();q.pop();
+            int val = dis[x][y];
+            for(int t =0;t<4;t++){
+                int nx = x+dx[t];
+                int ny = y+dy[t];
+                if(nx>=0 && nx<n && ny>=0 && ny<n && dis[nx][ny] > 1+val){
+                    q.push({nx,ny});
+                    dis[nx][ny] = dis[x][y]+1;
+                }
+            }
+        }
+
+        pq.push({dis[0][0],0,0});
+
+        while(!pq.empty()){
+            auto[a,b,c] = pq.top();
+            pq.pop();
+            if(b==n-1 && c==n-1) return a;
+            vis[b][c] = true;
+            for(int i =0;i<4;i++){
+                int nx = b+dx[i];
+                int ny = c+dy[i];
+                if(nx>=0 && ny>=0 && nx<n && ny<n && !vis[nx][ny]){
+                    int s = min(a,dis[nx][ny]);
+                    vis[nx][ny] = true;
+                    pq.push({s,nx,ny});
+                }
+            }
+        }
+
+        return -1;
     }
 };
