0b114b8c6f30a17ec5c83ccb6e25c05bef612a37
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-07-03 23:12:05 +0530

    Time: 7 ms (92.46%), Space: 32.7 MB (47.81%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -6,19 +6,21 @@ public:
         int n = grid.size(),m=grid[0].size();
         vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
         dis[0][0] = grid[0][0];
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({grid[0][0],0*m+0});
-        while(!pq.empty()){
-            auto [w,y] = pq.top();
-            int x = y/m;
-            y = y%m;
-            pq.pop();
-            if(w > dis[x][y]) continue;
+        deque<pair<int,int>> dq;
+        dq.push_front({0,0});
+        while(!dq.empty()){
+            auto [x,y] = dq.front();
+            dq.pop_front();
             for(int i =0;i<4;i++){
                 int nx = x+dx[i],ny = y+ dy[i];
                 if(nx>=0 && nx <n && ny >=0 && ny < m && dis[x][y]+grid[nx][ny]<dis[nx][ny]){
+                    if(grid[nx][ny]){
+                        dq.push_back({nx,ny});
+                    }
+                    else{
+                        dq.push_front({nx,ny});
+                    }
                     dis[nx][ny] = dis[x][y]+grid[nx][ny];
-                    pq.push({dis[nx][ny],nx*m+ny});
                 }
             }
         }
