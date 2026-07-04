3a0dc08fa0bee379542f343a3b3eaa3a1af38fd7
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-07-04 14:15:41 +0530

    Time: 107 ms (26.77%), Space: 135 MB (47.49%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    int solve_bfs(vector<vector<pair<int,int>>>& adj){
-        int ans = INT_MAX;
-        int n = adj.size();
-        vector<bool> vis(n);
-        queue<int> q;
-        q.push(0);
-        vis[0] = true;
-        while(!q.empty()){
-            int node = q.front();q.pop();
-            vis[node] = true;
-            for(auto& [c,nbr] : adj[node]){
-                ans = min(ans,c);
-                if(!vis[nbr]) q.push(nbr);
-            }
-        }
-        return ans;
-
-    }
-    int minScore(int n, vector<vector<int>>& roads) {
+    int minScore(int n, vector<vector<int>>& roads){
         vector<vector<pair<int,int>>> adj(n);
         for(auto& it : roads){
             adj[it[0]-1].push_back({it[2],it[1]-1});
             adj[it[1]-1].push_back({it[2],it[0]-1});
         }
-        return solve_bfs(adj);
+        queue<int> q;
+        q.push(0);
+        vector<bool> vis(n);
+        vis[0]=true;
+        int ans = INT_MAX;
+        while(!q.empty()){
+            auto node = q.front();q.pop();
+            vis[node] = true;
+            for(auto& [c,nbr] : adj[node]){
+                ans = min(c,ans);
+                if(!vis[nbr]){
+                    q.push(nbr);
+                }
+            }
+        }
+        return ans;
     }
 };
