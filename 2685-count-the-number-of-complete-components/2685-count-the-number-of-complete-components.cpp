43ca086d377f7121d120b86f1480b7414191cf31
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-07-11 13:09:19 +0530

    Time: 87 ms (14.04%), Space: 136.9 MB (33.54%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,32 +1,58 @@
 class DSU{
     public:
     int size;
-    vector<int> rank,parent;
-        DSU(int n, vector<vector<int>>& adj){
+    vector<int> edgCnt,verCnt;
+    vector<int> rnk,parent;
+        DSU(int n){
             size = n;
-            rank.resize(n,0);
-            parent.resize(n);
+            rnk.resize(n,0);
+            parent.resize(n,0);
             iota(parent.begin(),parent.end(),0);
         }
         
         int find(int i){
-            int root = parent[i];
-            if(root != parent[root]) parent[i] = find(root);
-            return root;
+            if(i != parent[i]) parent[i] = find(parent[i]);
+            return parent[i];
         }
 
         void unite(int i, int j){
             int irep = find(i);
             int jrep = find(j);
             if(irep == jrep) return;
-            if(rank[irep]<rank[jrep]){
-                rank[jrep] = rank[irep];
-            } else if(rank[irep]>rank[jrep]){
-                rank[irep] = rank[jrep];
+            if(rnk[irep]<rnk[jrep]){
+                parent[irep] = jrep;
+            } else if(rnk[irep]>rnk[jrep]){
+                parent[jrep] = irep;
             }
             else{
-                rank[irep]++;
+                parent[jrep] = irep;
+                rnk[irep]++;
             }
+        }
+        int countCompleteness(int n,vector<vector<int>>& adj){
+            edgCnt.assign(n,0);
+            verCnt.assign(n,1);
+            int ans = 0;
+            for(auto& it : adj){
+                int r1 = find(it[0]),r2 = find(it[1]);
+                if(r1 != r2){
+                    unite(it[0],it[1]);
+                    int root = find(it[0]);
+                    edgCnt[root] = edgCnt[r1]+edgCnt[r2]+1;
+                    verCnt[root] = verCnt[r1]+verCnt[r2];
+                }
+                else{
+                    edgCnt[r1]++;
+                }
+            }
+            for(int i =0;i<n;i++){
+                if(find(i) == i){
+                    int v = verCnt[i];
+                    int e = edgCnt[i];
+                    if(v*(v-1)/2 == e) ans++;
+                }
+            }
+            return ans;
         }
 };
 class Solution {
@@ -57,11 +83,13 @@ public:
         return ans;
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        DSU dsu(n);
         vector<vector<int>> adj(n);
         for(auto it: edges){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        return bfs(adj,n);
+        return dsu.countCompleteness(n,edges);
+        // return bfs(adj,n);
     }
 };
