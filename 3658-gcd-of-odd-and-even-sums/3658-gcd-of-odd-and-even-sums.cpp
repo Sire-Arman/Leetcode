c528dfb0957f32a70b64571c7d98a83393b0d228
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-07-15 19:07:55 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -6,7 +6,6 @@ public:
     int gcdOfOddEvenSums(int n) {
         int odd = (n*(2*1+(n-1)*2))/2;
         int even = (n*(2*2+(n-1)*2))/2;
-        cout<<odd<<' '<<even;
         return gcd(odd,even);
     }
 };
