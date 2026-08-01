bc47184152de7d61ce5571bfa8e427f1c48c45a8
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-08-01 18:53:07 +0530

    Time: 101 ms (13.18%), Space: 9.5 MB (96.48%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-   int play(vector<int> &nums, int l, int r, int turn, int score){
-       if(l==r) return score += turn*nums[l];
-       int ans;
-       if(turn == 1){
-           ans = max(nums[l]+play(nums,l+1,r,(turn*-1), score),
-                      nums[r]+play(nums,l,r-1,(turn*-1),score));
-       }
-       else{
-           ans = min(-nums[l]+play(nums,l+1,r,(turn*-1), score),
-                      -nums[r]+play(nums,l,r-1,(turn*-1),score));
-       }
-       return ans;
-   } 
+    int solve(vector<int>& nums,int l, int r, int p, int turn){
+        if(l==r){
+            return p += turn*nums[l];
+        }
+        int ans;
+        if(turn==1){
+            ans = max(nums[l] + solve(nums,l+1,r,p,turn*-1),nums[r] + solve(nums,l,r-1,p,turn*-1));
+        }
+        else{
+            ans = min(-nums[l]+solve(nums,l+1,r,p,turn*-1),-nums[r] + solve(nums,l,r-1,p,turn*-1));
+        }
+        return ans;
+    }
     bool predictTheWinner(vector<int>& nums) {
-        return play(nums,0,nums.size()-1,1,0)>=0;
+        return solve(nums,0,nums.size()-1,0,true)>=0;
     }
 };
