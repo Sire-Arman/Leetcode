2ea630bf2048ea765390b57da10e3c26c4d4b5de
Arman Siddiqui <105731322+Sire-Arman@users.noreply.github.com>
2026-08-09 15:52:52 +0530

    Time: 44 ms (20.07%), Space: 12 MB (68.94%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-   int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
-        
-        vector<vector<int>> dp(n, vector<int>(n + 1, 0));
-        vector<int> suffixSum(n, 0);
-        suffixSum[n - 1] = piles[n - 1];
-        
-        for (int i = n - 2; i >= 0; i--) {
-            suffixSum[i] = suffixSum[i + 1] + piles[i];
-        }
-        
-        for (int i = n - 1; i >= 0; i--) {
-            for (int m = 1; m <= n; m++) {
-                if (i + 2 * m >= n) {
-                    dp[i][m] = suffixSum[i];
-                } else {
-                    for (int x = 1; x <= 2 * m; x++) {
-                        dp[i][m] = max(dp[i][m], suffixSum[i] - dp[i + x][max(m, x)]);
-                    }
-                }
+    int dp[101][101][2];
+    // vector<vector<vector<int>>> dp;
+    int solve(vector<int>& p, int i, int M, bool turn, vector<int>& prefix){
+        if(i>=p.size()) return 0;
+        if(dp[i][M][turn] != 0) return dp[i][M][turn];
+        int ans=turn?0:INT_MAX;
+        int lim = p.size()-i;
+        for(int j=1;j<=min(lim,2*M);j++){
+            int temp=0;
+            if(turn){
+                int t = max(M,j);
+                temp = prefix[j+i]-prefix[i] + solve(p,j+i,t,!turn,prefix);
+                ans = max(ans,temp);
+            }
+            else{
+                int t = max(M,j);
+                temp = solve(p,j+i,t,!turn,prefix);
+                ans = min(ans,temp);
             }
         }
-        
-        return dp[0][1];
+        return dp[i][M][turn] = ans;
+    }
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        memset(dp,INT_MIN,sizeof(dp));
+        // dp.assign(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
+        vector<int> prefix(n+1,0);
+        for(int i = 1;i<=n;i++){
+            prefix[i] = prefix[i-1]+piles[i-1];
+        }
+        int M = 1;
+        int ans =0;
+        return solve(piles,0,M,true,prefix);
     }
 };
